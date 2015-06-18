5a021eaa928c1c8b348276af841c94150709b2e3
alivebao <641572831@qq.com>
2015-06-18 20:04:01 +0800

    set the position of String at round's center

--- README.md
+++ README.md
@@ -5,21 +5,22 @@
 一个仿[代码家的数字进度条](https://github.com/daimajia/NumberProgressBar)写的圆形进度条控件
 
 ---
-
-使用方法：
+IDE:Eclipse    
+    
+使用方法：    
 
 ```java
-	  <com.miao.roundprogressbar.RoundProgressBar
+	<com.miao.roundprogressbar.RoundProgressBar
         android:id="@+id/roundbar1"
         style="@style/RoundProgressBar_Default"
-        android:layout_width="200px"
-        android:layout_height="200px"
+        android:layout_marginTop="100dp"
+        android:layout_width="100dp"
+        android:layout_height="100dp"
         android:layout_gravity="center"
-        android:padding="20dp"
         custom:progress_current="0" />
 ```	
 
 效果图：     
 ![效果图](pic.jpeg)        
       
-IDE:Eclipse    
+
--- src/com/miao/roundprogressbar/MainActivity.java
+++ src/com/miao/roundprogressbar/MainActivity.java
@@ -1,8 +1,10 @@
 package com.miao.roundprogressbar;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 	private RoundProgressBar mRound;
@@ -11,10 +13,12 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);// 去掉标题栏
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);// 去掉信息栏
 		setContentView(R.layout.activity_main);
 
 		mRound = (RoundProgressBar) findViewById(R.id.roundbar1);
-
 		new Thread(new Runnable() {
 
 			@Override
